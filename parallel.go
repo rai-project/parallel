bfe1cc6a22d13b08fe1840928e7118e2140dbfdf
Abdul Dakkak <abduld@wolfram.com>
2017-07-30 11:01:44 -0400

    continue work

--- parallel.go
+++ parallel.go
@@ -3,14 +3,17 @@ package parallel
 import (
 	"runtime"
 	"runtime/debug"
+	"sync"
 
 	"github.com/rai-project/uuid"
 )
 
 type Parallel interface {
 	Start() Parallel
+	Wait()
 	Stop()
 	Add(Task)
+	Run()
 }
 
 type Task interface {
@@ -23,6 +26,23 @@ type ParallelImpl struct {
 	tasks       chan Task
 	maxRoutines int
 	looping     bool
+	wg          sync.WaitGroup
+}
+
+type NonCancelableTask struct {
+}
+
+func (NonCancelableTask) Cancelable() bool {
+	return false
+}
+
+type NonCancelableTaskFunc func()
+
+func (NonCancelableTaskFunc) Cancelable() bool {
+	return false
+}
+func (f NonCancelableTaskFunc) Run() {
+	f()
 }
 
 func (parallel *ParallelImpl) loop() {
@@ -36,6 +56,8 @@ func (parallel *ParallelImpl) loop() {
 			go parallel.loop()
 		}
 	}()
+
+	defer parallel.wg.Done()
 
 	for parallel.looping {
 		select {
@@ -53,6 +75,7 @@ func (parallel *ParallelImpl) loop() {
 
 func (p *ParallelImpl) Add(task Task) {
 	p.tasks <- task
+	p.wg.Add(1)
 }
 
 func (p *ParallelImpl) Start() Parallel {
@@ -69,8 +92,15 @@ func (p *ParallelImpl) Stop() {
 	close(p.tasks)
 }
 
+func (p *ParallelImpl) Wait() {
+	p.wg.Wait()
+}
+func (p *ParallelImpl) Run() {
+	p.Start().Wait()
+}
+
 func New(max int) Parallel {
-	if max == -1 {
+	if max <= 0 {
 		max = runtime.NumCPU()
 	}
 	return &ParallelImpl{
